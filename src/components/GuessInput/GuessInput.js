c1bb8d2318881382c1576b3fc75a5b378e7765e8
Julia <julia@Noutbuk-Julia.local>
2025-12-10 16:09:16 +0200

    added validation

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -5,6 +5,7 @@ const GuessInput = ({ onSubmit }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     onSubmit(word);
     setWord("");
   };
@@ -13,10 +14,13 @@ const GuessInput = ({ onSubmit }) => {
     <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
       <input
+        required
         id="guess-input"
         type="text"
         minLength={5}
         maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
         value={word}
         onChange={(e) => setWord(e.target.value.toUpperCase())}
       />
